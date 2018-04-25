c75089cd6994bf2a946f4aef6e156807fc683e94
NikhilPr95 <nikhill.prabhu@gmail.com>
2018-04-26 01:47:05 +0530

    Adds jupyter notebook, takes input and output text as data for lstm

--- complexity_classification.py
+++ complexity_classification.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense
@@ -7,19 +6,23 @@ from keras.layers.embeddings import Embedding
 from keras.preprocessing import sequence
 import keras.utils
 
+import nltk
 import glob
 import os.path
 import pprint
 
 
+def tokenize_and_lower(string):
+	return " ".join(nltk.word_tokenize(string)).lower()
+	
 def load_data():
 	'''
 	reads all question files and returns - 
-	X, y --> q_text, label
+	q_text, input, output, label
 	'''
 
 	complexities = dict()
-	complexity_file = open("questions-complexity.csv")
+	complexity_file = open("questions-complexity.csv", encoding='utf-8')
 	complexity_file.readline()
 
 	for line in complexity_file:
@@ -28,26 +31,36 @@ def load_data():
 	complexity_file.close()
 
 
-	question_files = glob.glob("./questions/*.txt")	
+	question_files = sorted(glob.glob("./questions/*.txt"))
 	questions = []
+	inputs = []
+	outputs = []
 	labels = []
 
 	for f in question_files:
-		handle = open(f)
+		handle = open(f, encoding='utf-8')
 		text = handle.read()
 		handle.close()
-		text = text.split("\n\n")[2]	### + [3] and [4] for Input and Output requirement text
-
-		### replace these with robust nltk fn calls if possible
-		text = text.replace("."," ")
-		text = text.replace(","," ")
-		text = text.replace("!"," ")
-		text = text.lower()
-		#######################################################
-		labels.append(complexities[os.path.basename(f).strip(".txt")])
-		questions.append(text)
+		text_split = text.split("\n\n")
+		question = text_split[2]	### + [3] and [4] for Input and Output requirement text
+		input = text_split[3]
+		output = text_split[4]
 		
-	return questions, labels
+		# Removes 'Input' and 'Output' prefixes
+		input = input[len('Input'):]
+		output = output[len('Output'):]
+		
+		question = tokenize_and_lower(question)
+		input = tokenize_and_lower(input)
+		output = tokenize_and_lower(output)
+		
+		questions.append(question)
+		inputs.append(input)
+		outputs.append(output)
+		labels.append(complexities[os.path.basename(f).strip(".txt")])
+		
+	return questions, inputs, outputs, labels
+	
 def create_vocab(questions):
 	'''
 	takes list of questions (returned from load_data) and returns a list of words sorted in desending order of frequency
@@ -59,12 +72,11 @@ def create_vocab(questions):
 				word_freq[word] = 0
 			word_freq[word] += 1
 	top_words = sorted(word_freq, key = lambda w : word_freq[w], reverse = True)
-	global vocab_size
 	vocab_size = len(top_words)
-	return top_words
+	
+	return top_words, vocab_size
 
-def manipulate_data(questions, labels):
-	vocab = create_vocab(questions)
+def vectorize_data(questions, labels, vocab):
 	questions_vector = []
 	for ques in questions:
 		single_question_vector = []
@@ -80,8 +92,7 @@ def manipulate_data(questions, labels):
 	
 	return questions_vector, labels_vector
 	
-def build_model(X,y):
-	global max_question_length, embedding_vector_length, n_epochs
+def build_model(X, y, vocab_size, max_question_length, embedding_vector_length, n_epochs):
 	X = sequence.pad_sequences(X, maxlen=max_question_length)
 
 	model = Sequential()
@@ -95,10 +106,10 @@ def build_model(X,y):
 
 	model.fit(X, y, epochs=n_epochs, batch_size=64, verbose=1)
 
+	print('SUMMARY:', model.summary())
 	return model
 
-def test_model(model, X, y):
-	global max_question_length
+def test_model(model, X, y, max_question_length):
 	X = sequence.pad_sequences(X, maxlen=max_question_length)
 	scores = model.evaluate(X, y)
 	print("Accuracy: %.2f%%" % (scores[1]*100))
@@ -109,25 +120,16 @@ def predict(X):
 
 if __name__ == "__main__":
 	max_question_length = 300
-	vocab_size = 0
 	embedding_vector_length = 32
 	n_epochs = 5
 	
-	X, y = load_data()
-	X, y = manipulate_data(X, y)
+	questions, inputs, outputs, labels = load_data()
+	vocab, vocab_size = create_vocab(questions)
+	X, y = vectorize_data(questions, labels, vocab)
 	
-	model = build_model(X[:1700], y[:1700])
-	test_model(model, X[1700:], y[1700:])
+	X_train, y_train = X[:1700], y[:1700]
+	X_test, y_test = X[1700:], y[1700:]
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	print('length:', len(X))
+	model = build_model(X_train, y_train, vocab_size, max_question_length, embedding_vector_length, n_epochs)
+	test_model(model, X_test, y_test)
